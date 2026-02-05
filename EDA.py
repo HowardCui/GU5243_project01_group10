3e9b1a267aba203ec47d4af661ef45404ae565ce
HowardCui <HowardCui@users.noreply.github.com>
2026-02-04 20:56:28 -0500

    0204

--- EDA.py
+++ EDA.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python 3.8
+#!/usr/bin/env python 3.12
 # -*- coding: utf-8 -*-
-# time: 2026/02/02
+# time: 2026/02/04
 # name: Haowen Cui
 
+import sys
 import pandas as pd
+import requests
 
+print(f'python version: {sys.version}')
+data_path = r'https://catalog.data.gov/dataset/u-s-chronic-disease-indicators'
